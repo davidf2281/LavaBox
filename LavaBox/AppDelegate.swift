f9e5ba646429f94e81cbe56ed459371852032f99
David Fearon <david.fearon@sportlobster.com>
2016-07-26 08:34:11 +0100

    Initial Commit

---
+++ LavaBox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LavaBox
+//
+//  Created by David Fearon on 26/07/2016.
+//  Copyright © 2016 David Fearon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LavaBox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LavaBox
+//
+//  Created by David Fearon on 26/07/2016.
+//  Copyright © 2016 David Fearon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
