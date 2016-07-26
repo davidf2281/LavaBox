dc0c8a1da7e631524af425637824dfc96c5ef2c8
David Fearon <david.fearon@sportlobster.com>
2016-07-26 08:44:13 +0100

    Initial commit: create skeleton class structure.

---
+++ LavaBox/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  LavaBox
+//
+//  Created by David Fearon on 26/07/2016.
+//  Copyright © 2016 David Fearon. All rights reserved.
+//
+
+import Cocoa
+
+class Environment
+{
+
+}
