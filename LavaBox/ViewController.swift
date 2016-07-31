2f0d7826e8c737d0632f9b4bbda135c597d028bb
David Fearon <apple@davidfearon.com>
2016-07-31 21:26:09 +0100

    Tidy tidy tidy

--- LavaBox/ViewController.swift
+++ LavaBox/ViewController.swift
@@ -32,8 +32,10 @@ class ViewController: NSViewController
         //********************************************
         
         let box = Container(externalLength: 0.4, externalWidth: 0.297, externalHeight: 0.283, wallThickness: 0.02, thermalConductivity: 0.037)
+        
         // Calibrate for the real world by applying empirically derived data for energy loss at a given temperature differential
         box.calibrateForMeasuredEnergyLoss(energyLoss: 38469, time: 600, externalTemperature: 27.4, internalTemperature: 60)
+        
         let heater = Heater(maxOutputPower: heaterPower)
         
         let waterBody = WaterBody(length: 0.36, width: 0.26, height: 0.13, initialTemperature: initialWaterTemperature)
